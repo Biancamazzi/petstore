4f25425b6ba53e6bbc371cf85ca17b6317127d6b
Felipe Zica <felipevzica@gmail.com>
2022-05-10 20:38:21 -0300

    routes validadas com a tachi

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :services, only: [:index, :show] do
+    resources :user_cart, only: [:new, :create]
+  end
+  # resources :user_cart, only: [:index] do
+  #     member do
+  #   post :approved
+  #   post :denied
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
