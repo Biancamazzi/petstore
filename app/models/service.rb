8fcaea031078ae5d9332c196e339502b8eb367b2
BiancaMazzi <bianca.mazzi@hotmail.com>
2022-05-10 20:59:35 -0300

    correcting associations

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user, though: user_carts
   # has_many :user_carts
   validates :name, presence: true
   validates :description, presence: true
