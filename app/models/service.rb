920dcd5b252719771ec35388c7a2af5eb4058b82
DaviEng <84865158+DaviEng@users.noreply.github.com>
2022-05-10 21:07:56 -0300

    Revert "02 -Service Model"

--- app/models/service.rb
+++
@@ -1,7 +0,0 @@
-class Service < ApplicationRecord
-  # belongs_to :user, though: user_carts
-  # has_many :user_carts
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
-end
