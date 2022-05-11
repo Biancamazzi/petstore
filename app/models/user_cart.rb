4448d582b3a3dc69ef9031c815977039635b3e53
Renan Martins <renanmartins989@gmail.com>
2022-05-11 02:23:20 +0100

    user cart model com status de pagamento +

--- app/models/user_cart.rb
+++ app/models/user_cart.rb
@@ -2,9 +2,9 @@ class UserCart < ApplicationRecord
   STATUS = ['pending', 'approved', 'denied']
   belongs_to :user
   belongs_to :service
-#after_create :set_status
-# Definindo a variavel global 'STATUS' para o metodo 'set_status'
-# O metodo 'set_status' defini todos pagamentos quando criados para 'pending' por default
+  # after_create :set_status
+  # Definindo a variavel global 'STATUS' para o metodo 'set_status'
+  # O metodo 'set_status' defini todos pagamentos quando criados para 'pending' por default
 
   def set_status
     self.status = STATUS.first
