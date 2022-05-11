6f0b046176d149d0cdccf7f283368cdf09995725
Felipe Zica <felipevzica@gmail.com>
2022-05-11 17:49:45 -0300

    db migrate to pricecents and monetize to service model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_13_004431) do
+ActiveRecord::Schema.define(version: 2022_05_11_204630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,17 +49,6 @@ ActiveRecord::Schema.define(version: 2022_05_13_004431) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
-  end
-
-  create_table "user_carts", force: :cascade do |t|
-    t.text "status"
-    t.bigint "service_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "checkout_session_id"
-    t.index ["service_id"], name: "index_user_carts_on_service_id"
-    t.index ["user_id"], name: "index_user_carts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
