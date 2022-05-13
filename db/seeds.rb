98dae47ea659c2586b00bab0ba292d8e40c525a9
fvzica <76051745+fvzica@users.noreply.github.com>
2022-05-12 21:48:09 -0300

    seeds with 4 services - name - description and price

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+User_carts.destroy_all
+Services.destroy_all
+
+puts 'Creating services...'
+
+banho = Service.create!(
+name: "Banho",
+description: "Banho completo com os melhores produtos"
+price: "15000"
+)
+
+tosa = Service.create!(
+name: "Tosa",
+description: "Tosa especializada para a raca do seu pet"
+price: "20000"
+)
+
+passeio = Service.create!(
+name: "Passeio",
+description: "Passeio na rua por uma hora com nossos profissionais"
+price: "5000"
+)
+
+daycare = Service.create!(
+name: "Day Care",
+description: "Creche com o melhor ambiente para o seu melhor amigo"
+price: "30000"
+)
+puts "All done with services (4) - Banho, Tosa, Passeio, Daycare"
