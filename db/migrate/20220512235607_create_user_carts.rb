21a2a3bf8569daff8d21075dd26da911344baab3
Renan Martins <renanmartins989@gmail.com>
2022-05-13 01:00:21 +0100

    model user cart 2 done

---
+++ db/migrate/20220512235607_create_user_carts.rb
@@ -0,0 +1,11 @@
+class CreateUserCarts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_carts do |t|
+      t.text :status
+      t.references :service, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
