76281f341751c94a954bffc3b7eb727b73b74a3c
fvzica <76051745+fvzica@users.noreply.github.com>
2022-05-14 15:36:58 -0300

    user cart controller - create

--- app/controllers/user_cart_controller.rb
+++ app/controllers/user_cart_controller.rb
@@ -10,16 +10,25 @@ class UserCartController < ApplicationController
   # new provavelmente sera descartado por nao ter utilidade
 
   def create
-    @service = Service.find(params[:service_id])
-    @user_cart = UserCart.new(usercart_params)
-    @user_cart.service = @service
-    @user_cart.user = current_user
-    if @user_cart.save
-      redirect_to service_path(@user_cart.service)
-    else
-      render :new
-    end
+    service = Service.find(params[:service_id])
+    user_cart  = User_cart.create!(service: service, amount: service.price, state: :status, user: current_user)
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: service.name,
+        images: [service.photo_url],
+        amount: service.price_cents,
+        currency: 'brl',
+        quantity: 1
+      }],
+      success_url: user_cart_url(user_cart),
+      cancel_url: user_cart_url(user_cart)
+    )
+    user_cart.update(checkout_session_id: session.id)
+    redirect_to new_user_cart_payment_path(user_cart)
   end
+
+
 
   private
 
@@ -32,3 +41,4 @@ end
 # ao criar, passar os params no metodo create na criacao do usercart!
 # criar um usercart params!!!
 # **rever rotas new e cogitar um destroy para usercart(necessario)
+
