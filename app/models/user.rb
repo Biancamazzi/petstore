c740a4dd2bce98ab4f86677e4a63c13b6c7098d2
Davi Rocha <davipradorocha@gmail.com>
2022-05-17 20:12:30 -0300

    install start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :user_cart
+  has_many :services, through: :user_cart
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,32 @@
 # services = Service.create([{name: 'servico1', description: 'teste1'}, {name: 'servico2', description: 'teste2'}])
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# puts 'Cleaning database...'
-# # User_cart.destroy_all
-# Services.destroy_all
+puts 'Cleaning database...'
+Service.destroy_all
 
 puts 'Creating services...'
 
 banho = Service.create!(
   name: "Banho",
-  description: "Banho completo com os melhores produtos"
+  description: "Banho completo com os melhores produtos",
+  price_cents: 9000
 )
 
 tosa = Service.create!(
 name: "Tosa",
-description: "Tosa especializada para a raca do seu pet"
+description: "Tosa especializada para a raca do seu pet",
+price_cents: 3000
 )
 
 passeio = Service.create!(
 name: "Passeio",
-description: "Passeio na rua por uma hora com nossos profissionais"
+description: "Passeio na rua por uma hora com nossos profissionais",
+price_cents: 5000
 )
 
 daycare = Service.create!(
 name: "Day Care",
-description: "Creche com o melhor ambiente para o seu melhor amigo"
+description: "Creche com o melhor ambiente para o seu melhor amigo",
+price_cents: 6000
 )
 puts "All done with services (4) - Banho, Tosa, Passeio, Daycare"
